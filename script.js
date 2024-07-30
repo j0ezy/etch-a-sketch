2f235861607fd9b87e50e9a37642f74b72440647
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-07-30 18:56:30 +0200

    Create button for size
    Created a button in index html and an event
    listener for the button that generates grid size
    based on user input

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 const containerGrid = document.querySelector(".container")
+const sizeButton = document.querySelector(".btn")
 
-for(let i = 0; i < 16; i++){
-    const gridRow = document.createElement("div")
-    gridRow.setAttribute("class", "gridRow")
-    containerGrid.appendChild(gridRow)
-    for(let j = 0; j < 16; j++){
-        const gridBox = document.createElement("div")
-        gridBox.setAttribute("class", "box")
-        gridRow.appendChild(gridBox)
+sizeButton.addEventListener("click", () =>{
+
+    // Remove the grid
+    const gridRow = document.querySelectorAll(".gridRow")
+    for(let row of gridRow){
+        row.remove()
     }
-}
 
-const gridBox = document.querySelectorAll(".box")
+    // Get input from user on grid Size and Generate grid
+    while(true){
+        let gridSize = +prompt("Enter the size of grid (max 100)")
 
-for(let box of gridBox){
-    box.addEventListener("mouseover", () =>{
-        box.classList.toggle("onHover")
-    })
-}
+        if(Number.isInteger(gridSize) && gridSize <= 100 && gridSize > 0){
+
+            for(let i = 0; i < gridSize; i++){
+                const gridRow = document.createElement("div")
+                gridRow.setAttribute("class", "gridRow")
+                containerGrid.appendChild(gridRow)
+                for(let j = 0; j < gridSize; j++){
+                    const gridBox = document.createElement("div")
+                    gridBox.setAttribute("class", "box")
+                    gridRow.appendChild(gridBox)
+                }
+            }
+            break
+        }
+    }
+
+    // Hover effect over box
+    const gridBox = document.querySelectorAll(".box")
+    for(let box of gridBox){
+        box.addEventListener("mouseenter", () =>{
+            box.classList.toggle("onHover")
+        })
+    }
+})
+
+
+
+
